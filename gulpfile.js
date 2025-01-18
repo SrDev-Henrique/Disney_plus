88c2fb6ea937cfa8168bb79753c33362fe25f571
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-01-18 00:51:53 -0300

    add hero section

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import gulpSass from 'gulp-sass';
 import * as sass from 'sass';
 import sourceMaps from "gulp-sourcemaps";
+import imagemin from "gulp-imagemin";
 
 const sassCompiler = gulpSass(sass);
 
@@ -18,18 +19,20 @@ export function compileSass() {
     .pipe(gulp.dest("./dist/css"));
 }
 
-export function styles() {
-    return gulp
-      .src("./src/styles/style.scss")
-      .pipe(
-        gulpSass({
-          outputStyle: "compressed",
-          implementation: sass,
-        })
-      )
-      .pipe(gulp.dest("./dist/css"));
+export function compressImages() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
 }
 
-export default function () {
-    gulp.watch("./src/styles/*.scss", gulp.parallel(compileSass));
+export function watchFiles() {
+  gulp.watch('./src/styles/**/*.scss', compileSass);
+
+  gulp.watch('./src/images/**/*', compressImages);
 }
+
+export default gulp.series(
+  gulp.parallel(compileSass, compressImages),
+  watchFiles
+);
