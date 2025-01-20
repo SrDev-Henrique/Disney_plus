62957c2112b89bb23762cbd4ac36365ce855e04a
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-01-20 17:52:05 -0300

    add compressJs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ import gulpSass from 'gulp-sass';
 import * as sass from 'sass';
 import sourceMaps from "gulp-sourcemaps";
 import imagemin from "gulp-imagemin";
+import GulpUglify from "gulp-uglify";
 
 const sassCompiler = gulpSass(sass);
 
@@ -26,13 +27,21 @@ export function compressImages() {
     .pipe(gulp.dest("./dist/images"));
 }
 
+export function compressJs() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(GulpUglify())
+    .pipe(gulp.dest("./dist/js"));
+}
+
 export function watchFiles() {
   gulp.watch('./src/styles/**/*.scss', compileSass);
 
   gulp.watch('./src/images/**/*', compressImages);
+
+  gulp.watch('./src/scripts/*.js', compressJs);
 }
 
 export default gulp.series(
-  gulp.parallel(compileSass, compressImages),
-  watchFiles
+  gulp.parallel(compileSass, compressImages, compressJs)
 );
